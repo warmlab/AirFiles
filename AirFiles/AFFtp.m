2431e4c405ecc75b462fa937ca7840b7ccc0223d
WarmLab <warmlab@me.com>
2012-10-26 17:54:38 +0800

    modify protocol

---
+++ AirFiles/AFFtp.m
@@ -0,0 +1,18 @@
+//
+//  AFftp.m
+//  AirFiles
+//
+//  Created by test on 10/19/12.
+//  Copyright (c) 2012 Websense. All rights reserved.
+//
+
+#import "AFFtp.h"
+
+@implementation AFFtp
+
+- (void)stream:(NSStream *)aStream handleEvent:(NSStreamEvent)eventCode
+{
+    NSLog(@"ddddd");
+}
+
+@end
